efa7ef7b0bfae4b74f37cd7120bd5620ce150223
H. Raese <h.raese@ufg.uni-kiel.de>
2020-02-05 18:41:50 +0100

    - erste Schritte

--- Playground/Formeln.R
+++ Playground/Formeln.R
@@ -1,4 +1,39 @@
+tbl <- read.csv("../testdata_input", sep = "")
 
+vec_indv <- unique(tbl$Individual)
+
+i <- 1
+
+for (i in i:length(vec_indv)){
+  df_knochen <- subset(tbl, subset=tbl$Individual == vec_indv[i])
+  
+  # R1 (müsste das so aussehen?)
+  if ("R1" %in% df_knochen$variable){
+  R1 <- df_knochen$value[df_knochen$variable == "R1"]
+  } else if (("R1_r" %in% df_knochen$variable) & !("R1_l" %in% df_knochen$variable)){
+    R1 <- df_knochen$value[df_knochen$variable == "R1_r"]
+  } else if (("R1_l" %in% df_knochen$variable) & !("R1_r" %in% df_knochen$variable)){
+    R1 <- df_knochen$value[df_knochen$variable == "R1_l"]
+  } else if (("R1_l" %in% df_knochen$variable) & ("R1_r" %in% df_knochen$variable)){
+    R1 <- (((df_knochen$value[df_knochen$variable == "R1_l"])+(df_knochen$value[df_knochen$variable == "R1_r"]))/2)
+  }
+ 
+  # R2 (müsste das so aussehen?)
+  if ("R2" %in% df_knochen$variable){
+    R2 <- df_knochen$value[df_knochen$variable == "R2"]
+  } else if (("R2_r" %in% df_knochen$variable) & !("R2_l" %in% df_knochen$variable)){
+    R2 <- df_knochen$value[df_knochen$variable == "R2_r"]
+  } else if (("R2_l" %in% df_knochen$variable) & !("R2_r" %in% df_knochen$variable)){
+    R2 <- df_knochen$value[df_knochen$variable == "R2_l"]
+  } else if (("R2_l" %in% df_knochen$variable) & ("R2_r" %in% df_knochen$variable)){
+    R2 <- (((df_knochen$value[df_knochen$variable == "R2_l"])+(df_knochen$value[df_knochen$variable == "R2_r"]))/2)
+  }
+   
+  F1 <- df_knochen$value[df_knochen$variable == "F1"]
+  H1 <- df_knochen$value[df_knochen$variable == "H1"]
+  tbl_val <- rbind.data.frame(c(R1,F1,H1),deparse.level = 1)
+}
+  
 
 # Nach Vercellotti u.a. 2009
 # Ergebnis muss durch 10 dividiert werden, um cm zu erhalten!
