48a6e85db7aba740291cb496a6ed15f06d3df383
Christoph Rinne <crinne@ufg.uni-kiel.de>
2020-02-07 18:48:17 +0100

    reading correct table headings from file, stop data processing in case of wrong headings.

--- R/readuserdata.R
+++ R/readuserdata.R
@@ -5,10 +5,10 @@ library(reshape2)
 
 # read user table
 td<-read.table('testdata.tab',sep='\t', head=T)
-td
+#td
 
 # check variable name / column headings
-user_measures <- colnames(td[-1])
+user_measures <- colnames(td)
 
 ## column headings: syntax standardisation
 user_measures <- gsub("-", "_", user_measures)
@@ -17,17 +17,25 @@ user_measures <- tolower(user_measures)
 #Uppercase first character
 user_measures <- gsub("(^|[[:space:]])([[:alpha:]])","\\1\\U\\2",user_measures,perl=TRUE)
 
+#for test purposes only
+# user_measures <- c(user_measures, "test")
+
 ## column headings: check for non specified labels
-  wrong_measures <- NULL
+measures<-scan("./R/measures.txt", what="character", sep = "\n", skip=1)  
+wrong_measures <- NULL
 
 for (i in user_measures){
   if (!(i %in% measures)) {
     wrong_measures <- c(wrong_measures,i)
   }
 }
-wrong_measures
 
+if (length(wrong_measures)>=1) {
+  stop (paste("The following column headings do not match the requirements:", wrong_measures, sep="\n"))
+}
+  
 # transform user data into list
 td_<-na.omit(melt(td,id="Individual"))
+message
 
 # Check values
