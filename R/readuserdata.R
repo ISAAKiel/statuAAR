61a8381ce86d46696f4630c892d0244a0185e064
Christoph Rinne <crinne@ufg.uni-kiel.de>
2020-02-29 20:39:50 +0100

    Introduced a grouping variable (grp). Individual (ind) and grouping var (grp) are optional now and set to "all" if NULL. In consequence some adjustments. Additional testdata added.

--- R/readuserdata.R
+++ R/readuserdata.R
@@ -27,11 +27,16 @@
 library(tidyverse)
 library (dplyr)
 
-# for test resons
+# for test reasons, will be deleted
 x<-read.table('testdata.tab',sep='\t', head=T)
+x<-read.table('testdata_indgrp.tab',sep='\t', head=T)
+x<-read.table('testdata_input.tab',sep='\t', head=T)
+x<-rbind(x,x)
 ds<-'table'
+ds<-'list'
 ind<-'Individual'
-grp<- NA
+ind<-NULL
+grp<- NULL
 tdl-> td
 
 # read user table
@@ -50,39 +55,48 @@ prep.body.hight = function (x, ds = 'table', ind, grp) {
   names(td)[which(names(td)==grp)]<-'grp'
   
   # check for duplicated identifiers (individuals)
-  if (ds == 'table') {
-    dupl_ind<-td$ind[duplicated(td$ind)]
-  } else if (ds == 'list') {
-   id<-gsub('l', '', td$variable)
-   id<-gsub('r', '', id)
-   id<-gsub('_', '', id)
-   id<-paste(tdl$ind, id, sep = '_')
-   dupl_ind<-names(table(id)[table(id)>2])
+  dupl_ind<-NULL
+  if (!is.null(ind)) {
+    if (ds == 'table') {
+      dupl_ind<-td$ind[duplicated(td$ind)]
+    } else if (ds == 'list'){
+      id<-gsub('_[LlRr]','',td$variable)
+     id<-paste(td$ind, id, sep = '_')
+     dupl_ind<-names(table(id)[table(id)>2])
+    }
   }
-  
   if (length(dupl_ind)>0) {
     stop(paste("Identifier for Individuals is not unique for:", 
                paste(dupl_ind, collapse=", "), 
                sep=" "))
   }
+  # if grouping (grp) is NULL all measures a set to group "all"
+  if (is.null(grp)) {
+    td<-cbind(grp=rep('all', nrow(td)),td)
+  } 
+  
+  # if individual identifyer (ind) is NULL all measures a set to ind "all"
+  if (is.null(ind)) {
+    td<-cbind(ind=rep('all', nrow(td)),td)
+  } 
   
   # transform tabled user data into a list
   if (ds=='table') {
-    tdl<-as_tibble(reshape2::melt(td, na.rm=TRUE, id=1))
+    tdl<-dplyr::as_tibble(reshape2::melt(td, na.rm=TRUE, id=c("ind", "grp")))
   } else {
-    tdl<-as_tibble(na.omit(td))
+    tdl<-dplyr::as_tibble(na.omit(td))
   }
   
   # check variable name / column headings
   # variable names: <blank> and "-" have been converted to "."
-  tdl[[2]]<-gsub("\\.", "_", tdl[[2]])
-  tdl[[2]]<- tolower(tdl[[2]])
+  tdl[[3]]<-gsub("\\.", "_", tdl[[3]])
+  tdl[[3]]<- tolower(tdl[[3]])
   # first character to upper case
-  tdl[[2]]<-gsub("(^|[[:space:]])([[:alpha:]])","\\1\\U\\2",tdl[[2]],perl=TRUE)
+  tdl[[3]]<-gsub("(^|[[:space:]])([[:alpha:]])","\\1\\U\\2",tdl[[3]],perl=TRUE)
   
   
   ## column headings: check for non specified labels
-  user_measures<-unique(tdl[[2]])
+  user_measures<-unique(tdl[[3]])
   measures<-scan("./R/measures.txt", what="character", sep = "\n", skip=1)  
   wrong_measures <- NULL
   
@@ -96,8 +110,8 @@ prep.body.hight = function (x, ds = 'table', ind, grp) {
     stop (paste("The following column headings do not match the requirements:",  
                 paste(wrong_measures, collapse=", "), sep = "\n"))
    }
-  
- # aggegate statistics for data check
+
+  # aggegate statistics for data check
   agg_measures<-data.frame(measure=character(), 
                          n=integer(), 
                          MinM=numeric(), 
@@ -110,15 +124,12 @@ prep.body.hight = function (x, ds = 'table', ind, grp) {
   
   for (i in 1:length(user_measures)) {
     agg_measures[i,] <- as.list(c(user_measures[i],
-           length(subset(tdl[[3]],tdl[[2]]==user_measures[i])),
-           as.vector(summary(subset(tdl[[3]],tdl[[2]]==user_measures[i])))))
-  }
-  for (cn in colnames(agg_measures[3:8])) {
-     
+           length(subset(tdl[[4]],tdl[[3]]==user_measures[i])),
+           as.vector(summary(subset(tdl[[4]],tdl[[3]]==user_measures[i])))))
   }
   agg_measures[,2:8] <- sapply(agg_measures[,2:8], as.numeric)
-  agg_measures<-as_tibble(agg_measures)
-  agg_measures<- mutate(agg_measures, 
+  agg_measures<-dplyr::as_tibble(agg_measures)
+  agg_measures<- dplyr::mutate(agg_measures, 
                         maxDiff2Mean = (agg_measures$MaxM - agg_measures$MinM) * 100/agg_measures$MedianM)
   if (any(agg_measures[,9]>1.5)) {
     warning (paste('We calculatet the max. Diff. for each measurement in relation to its mean.',
