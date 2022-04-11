f0bbba243a63fdadb30c7b19ea8117bcc1cd88c3
crinne <crinne@ufg.uni-kiel.de>
2022-04-11 11:56:43 +0200

    documenation works

--- R/breitinger_bach_1965.R
+++ R/breitinger_bach_1965.R
@@ -1,23 +1,23 @@
 #' @name breitinger_bach_1965
 #'
 #' @title Stature estimation according to Breitinger 1938 & Bach 1965.
-#' 
-#' @description 
+#'
+#' @description
 #' Stature estimation (mm) based on the mean of different regression calculations,
-#' separated  by sex Breitinger 1938, Bach 1965).
+#' separated  by sex (Breitinger 1938, Bach 1965).
 #' Bone measures used: Hum2, Hum1, Rad1b, Fem1, Tib1b.
-#' 
+#'
 #' If bone measures for left and right are provided the mean value will be used,
 #' but for statistic information 2 bones will be counted (n_measures).
 #' If sex is indet. the mean of male and female stature estimation is given.
-#' Breitinger (1938) does not show the regression equation for Hum1, but the 
-#' equation can be derived from the values given in the table p. 272 
+#' Breitinger (1938) does not show the regression equation for Hum1, but the
+#' equation can be derived from the values given in the table p. 272
 #' (see Siegmund 2010, p. 112). In contrast, Bach (1965) gives both equations.
-#'  In addition, Bach (1965, 20) states to use only one of the Humeri measures 
-#'  for estimation (Hum1 or Hum2 or the mean of both) to avoid inadmissable 
-#'  multiplication of the bone within the stature estimation. 
+#'  In addition, Bach (1965, 20) states to use only one of the Humeri measures
+#'  for estimation (Hum1 or Hum2 or the mean of both) to avoid inadmissable
+#'  multiplication of the bone within the stature estimation.
 #'
-#' Returns a data.frame with: 
+#' Returns a data.frame with:
 #'  \itemize{
 #'    \item{ ind: individual identifyer (rownames), }
 #'    \item{ sex: as provided for calculation: m, f, indet.}
@@ -26,46 +26,48 @@
 #'    \item{ female (stature): columns with alternative stature for three sex classes, }
 #'    \item{ male (stature), }
 #'    \item{ indet. (stature) and}
-#'    \item{ n_measures: number of bone measures included: 
+#'    \item{ n_measures: number of bone measures included:
 #'              e.g. 2 Fem2 (left, right) + 1 Tib1}
 #'  }
-#' 
+#'
 #' @param df data.frame, containing informations on individual, bone and measurement
-#'  
+#'
 #' @return data.frame, containing one data.frame with all calculated indices for every individual
-#'           
+#'
 #' @author Christoph Rinne \email{crinne@@ufg.uni-kiel.de}
-#' 
+#'
 #' @importFrom Rdpack reprompt
-#' 
+#'
 #' @references
-#'   \insertRef{Rpack:bibtex}{Rdpack}
-#'   
-#'   \insertRef{auerbachStatureEstimationFormulae2010}
-#'    
+#'   \insertRef{bach1965}{statuAAR}
+#'
+#'   \insertRef{breitinger1937}{statuAAR}
+#'
+#'   \insertRef{siegmund2010}{statuAAR}
+#'
 #' @examples
-#' 
+#'
 #'@export
 
 library(dplyr)
 
 breitinger_bach_1965 <- function(df){
-  
+
   df$variable<-gsub("([rl]$)","", df$variable) # laterality not needed
   # aggregate values for each measure and individual
   options(dplyr.summarise.inform = FALSE)
-  df %>%  
-    group_by(Ind, Sex, Group, variable) %>% 
+  df %>%
+    group_by(Ind, Sex, Group, variable) %>%
     summarise(mean.value = mean(value), n = n()) -> df
-  
+
   vec_indv <- unique(df$Ind) # extract names and quantity of unique individuals
-  
+
   # Initialize data frame for later storage of different mean body heights
   val_indv <- as.data.frame(matrix(ncol=8, nrow=length(vec_indv)), row.names=vec_indv)
   colnames(val_indv) <-c("sex", "group", "stature", "bone", "female", "male", "indet", "n_measures")
   val_indv$sex <- factor(val_indv$sex, labels = c("m", "f", "indet"), levels = c(1,2,3))
-  
-  # check available values for different variables needed for 
+
+  # check available values for different variables needed for
   for (i in 1:length(vec_indv)){
     df_bones <- subset(df, subset=df$Ind == vec_indv[i])
     # Get measure values needed
@@ -76,7 +78,7 @@ breitinger_bach_1965 <- function(df){
     Tib1b <- df_bones$mean.value[df_bones$variable=="Tib1b"]
 
     # document bone measures and number used for calculation
-    
+
     bone <- c()
     n_measures <- 0
     if (length(Hum2)>0) {
@@ -97,7 +99,7 @@ breitinger_bach_1965 <- function(df){
     if (length(Tib1b)>0) {
       bone <- append(bone, "Tib1b")
       n_measures <- n_measures + df_bones$n[df_bones$variable=="Tib1b"]
-    } 
+    }
 
     # Calculate the different indices for male
     measures.m <- c()
@@ -106,18 +108,18 @@ breitinger_bach_1965 <- function(df){
     measures.m <- append (measures.m, (Rad1b / 10) * 2.968 + 97.09)
     measures.m <- append (measures.m, (Fem1 / 10) * 1.645 + 94.31)
     measures.m <- append (measures.m, (Tib1b / 10) * 1.988 + 95.59)
-    
-    # Calculate the different indices for female 
+
+    # Calculate the different indices for female
     measures.f <- c()
     measures.f <- append (measures.f, (Hum1 / 10) * 2.121 + 98.38)
-    measures.f <- append (measures.f, (Hum2 / 10) * 2.121 + 99.44) 
+    measures.f <- append (measures.f, (Hum2 / 10) * 2.121 + 99.44)
     measures.f <- append (measures.f, (Rad1b / 10) * 1.925 + 116.89)
     measures.f <- append (measures.f, (Fem1 / 10) * 1.313 + 106.69)
     measures.f <- append (measures.f, (Tib1b / 10) * 1.745 + 95.91)
-    
+
     # Calculate the different indices for indet.
     measures.i <-(measures.m + measures.f)/2
-    
+
     # calculate mean of each measures group for statures
     # as the regression equations are calculated in cm the result is converted to mm
 
@@ -133,10 +135,10 @@ breitinger_bach_1965 <- function(df){
     val_indv$indet[i] <- statures[3]
     val_indv$n_measures[i] <- n_measures
   }
-  
+
   if (dim(val_indv)[1] == 0) {
     print("There is no usable bone measurement / indice available for the chosen formula")
   }
-  
+
   return(val_indv)
 }
