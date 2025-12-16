7ca8c5e00493939c8dae9fd928c376ac6f8da728
crinne <crinne@ufg.uni-kiel.de>
2025-12-16 08:07:36 +0100

    added list of formula with name and mesures

--- data-raw/data_prep.R
+++ data-raw/data_prep.R
@@ -38,6 +38,7 @@ TrotterGleser1952 <- read.csv(
 )
 usethis::use_data(TrotterGleser1952, overwrite = TRUE)
 
+# concordance of measure names
 measures.concordance <- read.csv(
   "data-raw/measures.concordance.tab",
   sep = "\t",
@@ -49,3 +50,20 @@ measures.concordance <- read.csv(
   colClasses = c(rep("character",3))
 )
 usethis::use_data(measures.concordance, overwrite = TRUE)
+
+# List of formula
+bb65 <- list(name = 'breitinger_bach_1965', measures = c('Hum2', 'Hum1', 'Rad1b', 'Fem1', 'Tib1b'))
+by89 <- list(name = 'byers_etal_1989', measures = c('Mt11'))
+fe90 <- list(name = 'feldesman_etal_1990', measures = c('Fem1'))
+ff96 <- list(name = 'formicola_franceschi_1996', measures = c('Fem2+Tib1', 'Fem1', 'Tib1', 'Hum1', 'Rad1'))
+mn09 <- list(name = 'maijanen_niskanen_2009', measures = c('Fem2+Tib1', 'Fem1+Tib1', 'Fem2', 'Fem1', 'Tib1', 'Hum1', 'Rad1', 'Fib1', 'Uln1'))
+ol78 <- list(name = 'olivier_etal_1978', measures = c('Hum2', 'Hum1', 'Rad1b', 'Fem1', 'Tib1b'))
+pe99 <- list(name = 'pearson_1899', measures = c('Hum1', 'Rad1', 'Fem1', 'Tib1b', 'Tib1a'))
+ra08 <- list(name = 'raxter_etal_2008', measures = c('Fem1', 'Fem1+Tib1a', 'Fem2', 'Fem2+Tib1a', 'Fem2+Tib1b', 'Hum1', 'Hum1+Rad1', 'Rad1', 'Tib1a', 'Tib1b'))
+sj90 <- list(name = 'sjovold_1990', measures = c('Hum1', 'Rad1', 'Rad1b', 'Uln1', 'Fem1', 'Fem2', 'Tib1', 'Tib1b', 'Fib1'))
+te50 <- list(name = 'telkkae_1950', measures = c('Hum1', 'Rad2', 'Uln2', 'Fem1', 'Tib1', 'Fib1'))
+tg01 <- list(name = 'trotter_gleser_1952_an', measures = c('Fem1', 'Tib1b', 'Fib1', 'Uln1', 'Rad1', 'Hum1'))
+tg02 <- list(name = 'trotter_gleser_1952_aw', measures = c('Fem1', 'Tib1b', 'Fib1', 'Uln1', 'Rad1', 'Hum1'))
+ve09 <- list(name = 'vercellotti_etal_2009', measures = c('Fem2+Tib1', 'Fem2', 'Fem1', 'Tib1', 'Hum1+Rad1', 'Hum1', 'Rad1'))
+formula <- list(by89, fe90, ff96, mn09, ol78, pe99, ra08, sj90, te50, tg01, tg02, ve09)
+usethis::use_data(formula, overwrite = TRUE)
