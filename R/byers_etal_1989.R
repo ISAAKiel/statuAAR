482d2686b012a988b1a139abbf27b99bfa99163b
crinne <crinne@ufg.uni-kiel.de>
2025-12-16 17:29:58 +0100

    added sex and updated function

--- R/byers_etal_1989.R
+++ R/byers_etal_1989.R
@@ -3,16 +3,19 @@
 #' @title Calculate stature estimation according to: Byers et al 1989.
 #'
 #' @description
-#' Stature estimation (mm) based on a regression calculation of one bone,
-#' not separated  by sex (Byers et al 1989).
+#' Stature estimation (mm) based on a regression calculation of one Metatarsal 1
+#' only separated by sex (Byers et al 1989).
 #' Bone measures used: Os metatarsale I, apex of the capitulum to the midpoint
 #' of the articular surface of the base parallel to the longituninal axis of the bone.
 #'
 #' If bone measures for left and right are provided the mean value will be used,
 #' but for statistic information 2 bones will be counted (n_measures).
-#' The  correlation coefficient does not increase with sex differentiation and thus
-#' will be ignored. Based on the length of the Os metatarsale I of different individuals,
-#' the stature for males and females is calculated.
+#' For archaeological finds, group assignment (Euro-American or Afro-American)
+#' is not possible. In addition, the gender-specific estimates for Met1 offer the
+#' best accuracy. For this reason, only the following formulas are used:
+#' Combined data (indet): St = 634 + 16.8 (Metl),
+#' All males: St = 815 + 14.3 (Metl),
+#' All females: St = 783 + 13.9 (Metl).
 #'
 #' Returns a data.frame with:
 #' \itemize{
@@ -24,7 +27,7 @@
 #' \item{ male (stature), }
 #' \item{ indet. (stature) and}
 #' \item{ n_measures: number of bone measures included:
-#'              e.g. 2 Fem2 (left, right) + 1 Tib1}
+#'              e.g. 2 Met1 (left, right)}
 #' }
 #'
 #' @param df data.frame of type statuaar_data_table, containing informations on individual, bone and measurement.
@@ -41,44 +44,48 @@
 #'
 #' @export
 
-library(dplyr)
-
 byers_etal_1989 <- function(df){
-
   df$variable<-gsub("([rl]$)","", df$variable) # laterality not needed
+  # check if needed measures are present
+  needed <- getFormulaMeasures('byers_etal_1989')
+  if (!any(df$variable %in% needed)){
+    return("There is no usable bone measurement / indice available for the chosen formula.")
+  }
   # aggregate values for each measure and individual
-  options(dplyr.summarise.inform = FALSE)
-  df %>%
-    group_by(Ind, Sex, Group, variable) %>%
-    summarise(mean.value = mean(value), n = n()) %>%
-    as.data.frame -> df
+  df <- aggregate(value ~ Ind + Sex + variable,
+                  data = df,
+                  FUN = function(x) c(mean = mean(x), n = length(x)))
+  df <- do.call(data.frame, df)
 
   vec_indv <- unique(df$Ind) # extract names and quantity of unique individuals
 
   # Initialize data frame for later storage of different mean body heights
-  val_indv <- as.data.frame(matrix(ncol=8, nrow=length(vec_indv)), row.names=vec_indv)
-  colnames(val_indv) <-c("sex", "group", "stature", "bone", "female", "male", "indet", "n_measures")
+  val_indv <- as.data.frame(matrix(ncol=7, nrow=length(vec_indv)), row.names=vec_indv)
+  colnames(val_indv) <-c("sex", "stature", "bone", "female", "male", "indet", "n_measures")
   val_indv$sex <- factor(val_indv$sex, labels = c("m", "f", "indet"), levels = c(1,2,3))
 
   # check available values for different variables needed for
   for (i in 1:length(vec_indv)){
     df_bones <- subset(df, subset=df$Ind == vec_indv[i])
     # Get measure values needed
-    Mt11 <- df_bones$mean.value[df_bones$variable=="Mt11"]
+    Met1 <- df_bones$value.mean[df_bones$variable=="Met1"]
+    # document number of measures
+    n_measures <- df_bones$value.n[df_bones$variable=="Met1"]
     #Calculate
-    stature <- (Mt11 * 16.8) + 634
+    stature.m <- (Met1 * 14.3) + 815
+    stature.f <- (Met1 * 13.9) + 783
+    stature.i <- (Met1 * 16.8) + 634
 
-    stature <- round(stature, 0)
+    statures <- round(c(stature.m, stature.f, stature.i), 0)
 
     # write values into data frame of results
     val_indv$sex[i] <- unique(df_bones$Sex)
-    val_indv$stature[i] <- stature
-    val_indv$bone[i] <- "Mt11"
-    val_indv$group[i] <- unique(df_bones$Group)
-    val_indv$female[i] <- stature
-    val_indv$male[i] <- stature
-    val_indv$indet[i] <- stature
-    val_indv$n_measures[i] <- df_bones$n[df_bones$variable=="Mt11"]
+    val_indv$stature[i] <- statures[as.integer(unique(df_bones$Sex))]
+    val_indv$bone[i] <- "Met1"
+    val_indv$female[i] <- statures[2]
+    val_indv$male[i] <- statures[1]
+    val_indv$indet[i] <- statures[3]
+    val_indv$n_measures[i] <- n_measures
   }
 
   if (dim(val_indv)[1] == 0) {
