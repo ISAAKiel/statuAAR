cd3611f4fd349f9c3f8cdba4e363b3f1c8de9c6a
crinne <crinne@ufg.uni-kiel.de>
2025-12-18 14:42:51 +0100

    without dplyr & group

--- R/telkkae_1950.R
+++ R/telkkae_1950.R
@@ -40,34 +40,39 @@
 #'
 #' @export
 
-library(dplyr)
-
 telkkae_1950 <- function(df){
 
   df$variable<-gsub("([rl]$)","", df$variable) # laterality not needed
+
+  # check if needed measures are present
+  needed <- getFormulaMeasures('telkkae_1950')
+  if (!any(df$variable %in% needed)){
+    return("There is no usable bone measurement / indice available for the chosen formula.")
+  }
+
   # aggregate values for each measure and individual
-  options(dplyr.summarise.inform = FALSE)
-  df %>%
-    group_by(Ind, Sex, Group, variable) %>%
-    summarise(mean.value = mean(value), n = n()) -> df
+  df <- aggregate(value ~ Ind + Sex + variable,
+                  data = df,
+                  FUN = function(x) c(mean = mean(x), n = length(x)))
+  df <- do.call(data.frame, df)
 
   vec_indv <- unique(df$Ind) # extract names and quantity of unique individuals
 
   # Initialize data frame for later storage of different mean body heights
-  val_indv <- as.data.frame(matrix(ncol=8, nrow=length(vec_indv)), row.names=vec_indv)
-  colnames(val_indv) <-c("sex", "group", "stature", "bone", "female", "male", "indet", "n_measures")
+  val_indv <- as.data.frame(matrix(ncol=7, nrow=length(vec_indv)), row.names=vec_indv)
+  colnames(val_indv) <-c("sex", "stature", "bone", "female", "male", "indet", "n_measures")
   val_indv$sex <- factor(val_indv$sex, labels = c("m", "f", "indet"), levels = c(1,2,3))
 
   # check available values for different variables needed for
   for (i in 1:length(vec_indv)){
     df_bones <- subset(df, subset=df$Ind == vec_indv[i])
     # Get measure values needed
-    Hum1 <- df_bones$mean.value[df_bones$variable=="Hum1"]
-    Rad2 <- df_bones$mean.value[df_bones$variable=="Rad2"]
-    Uln2 <- df_bones$mean.value[df_bones$variable=="Uln2"]
-    Fem1 <- df_bones$mean.value[df_bones$variable=="Fem1"]
-    Tib1 <- df_bones$mean.value[df_bones$variable=="Tib1"]
-    Fib1 <- df_bones$mean.value[df_bones$variable=="Fib1"]
+    Hum1 <- df_bones$value.mean[df_bones$variable=="Hum1"]
+    Rad2 <- df_bones$value.mean[df_bones$variable=="Rad2"]
+    Uln2 <- df_bones$value.mean[df_bones$variable=="Uln2"]
+    Fem1 <- df_bones$value.mean[df_bones$variable=="Fem1"]
+    Tib1 <- df_bones$value.mean[df_bones$variable=="Tib1"]
+    Fib1 <- df_bones$value.mean[df_bones$variable=="Fib1"]
 
     # document bone measures and number used for calculation
 
@@ -75,27 +80,27 @@ telkkae_1950 <- function(df){
     n_measures <- 0
     if (length(Hum1)>0) {
       bone <- append(bone, "Hum1")
-      n_measures <- n_measures + df_bones$n[df_bones$variable=="Hum1"]
+      n_measures <- n_measures + df_bones$value.n[df_bones$variable=="Hum1"]
     }
     if (length(Rad2)>0) {
       bone <- append(bone, "Rad2")
-      n_measures <- n_measures + df_bones$n[df_bones$variable=="Rad2"]
+      n_measures <- n_measures + df_bones$value.n[df_bones$variable=="Rad2"]
     }
     if (length(Uln2)>0) {
       bone <- append(bone, "Uln2")
-      n_measures <- n_measures + df_bones$n[df_bones$variable=="Uln2"]
+      n_measures <- n_measures + df_bones$value.n[df_bones$variable=="Uln2"]
     }
     if (length(Fem1)>0) {
       bone <- append(bone, "Fem1")
-      n_measures <- n_measures + df_bones$n[df_bones$variable=="Fem1"]
+      n_measures <- n_measures + df_bones$value.n[df_bones$variable=="Fem1"]
     }
     if (length(Tib1)>0) {
       bone <- append(bone, "Tib1")
-      n_measures <- n_measures + df_bones$n[df_bones$variable=="Tib1"]
+      n_measures <- n_measures + df_bones$value.n[df_bones$variable=="Tib1"]
     }
     if (length(Fib1)>0) {
       bone <- append(bone, "Fib1")
-      n_measures <- n_measures + df_bones$n[df_bones$variable=="Fib1"]
+      n_measures <- n_measures + df_bones$value.n[df_bones$variable=="Fib1"]
     }
 
     # Calculate the different indices for male
@@ -128,7 +133,6 @@ telkkae_1950 <- function(df){
     val_indv$sex[i] <- unique(df_bones$Sex)
     val_indv$stature[i] <- statures[as.integer(unique(df_bones$Sex))]
     val_indv$bone[i] <- paste(bone, collapse = ", ")
-    val_indv$group[i] <- unique(df_bones$Group)
     val_indv$female[i] <- statures[2]
     val_indv$male[i] <- statures[1]
     val_indv$indet[i] <- statures[3]
