4826bf75dc583e0f5b358ecde23ca71510e8000c
crinne <crinne@ufg.uni-kiel.de>
2026-02-25 09:49:52 +0100

    code syntax

--- tests/check.stature.Bach1965.R
+++ tests/check.stature.Bach1965.R
@@ -13,17 +13,16 @@ dl.bach1965 <- prep.statuaar.data(Bach1965, d.form = "wide",
                measures.names = "short", sex = "sex", stats = FALSE)
 
 # Get stature estimations
-short<-getFormulaDataframe()[,1]
+short <- getFormulaDataframe()[,1]
 bb65.estimates <- getStature(short, dl.bach1965)
 bb65.estimates.df <- getStatureDataframe(bb65.estimates)
 
 # Merge the original published and the calculated stature.
 bb65.check <- merge(Bach1965[5], bb65.estimates.df,
-                    by.x='row.names', by.y = 'id')
+                    by.x = 'row.names', by.y = 'id')
 bb65.check$diff <- bb65.check$stature.x - bb65.check$stature.y
 
 # Calculate the difference.
-
 boxplot(data = bb65.check, diff~formula,
         main = "Stature (Bach 1965) and estimates",
         xlab = "Formula abbreviation (short)",
