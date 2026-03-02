aeb1dbd9df2c9ca0ce4671121d2fd108cdf0014b
crinne <crinne@ufg.uni-kiel.de>
2026-03-02 08:13:53 +0100

    added package reference testthat::

--- tests/testthat/test_measureconcordance.R
+++ tests/testthat/test_measureconcordance.R
@@ -4,6 +4,6 @@ test_that("Daten korrekt geladen", {
   measures.concordance <- read.table(system.file("extdata","measures.concordance.rollet1888.tab",
                                                package = "statuAAR"))
 
-  expect_true(!is.null(measures.concordance))
-  expect_true(nrow(measures.concordance) > 0)  # Überprüfe, ob die Datei nicht leer ist
+  testthat::expect_true(!is.null(measures.concordance))
+  testthat::expect_true(nrow(measures.concordance) > 0)  # Überprüfe, ob die Datei nicht leer ist
 })
