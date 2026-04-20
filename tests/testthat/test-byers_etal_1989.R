ff3a9c425aed3abc8054634ab3c8a2c595d9b7bd
crinne <crinne@ufg.uni-kiel.de>
2026-04-20 13:12:59 +0200

    new tests

---
+++ tests/testthat/test-byers_etal_1989.R
@@ -0,0 +1,47 @@
+test_that("byers_etal_1984 aggregates left/right values and computes stature correctly", {
+  df <- data.frame(
+    Ind = c("ind1", "ind1", "ind1"),
+    Sex = factor('m', 'm', 'm'), # 1 = m, 2 = f, 3 = indet
+    variable = c("MtI1l", "MtI1r", "Tib1"),
+    value = c(49, 51, 380)
+  )
+
+  res <- byers_etal_1989(df)
+
+  expect_s3_class(res, "data.frame")
+  expect_equal(rownames(res), "ind1")
+
+  # laterality removed, Fem1 averaged, Tib1 excluded
+  expect_equal(res["ind1", "bone"], "MtI1")
+
+  # n_measures counts both Fem1
+  expect_equal(res["ind1", "n_measures"], 2)
+
+  # expected values:
+  # MtI1 mean = (49 + 51) / 2 = 50
+  # Male:   (50 * 14.3) + 815 = 1530
+  # Female: (50 * 13.9) + 783 = 1478
+  # Indet:  (50 * 16.8) + 634 = 1474
+  expect_equal(res["ind1", "if_male"], 1530)
+  expect_equal(res["ind1", "if_female"], 1478)
+  expect_equal(res["ind1", "if_indet"], 1474)
+
+  # stature should follow the provided sex
+  expect_equal(res["ind1", "stature"], 1530)
+})
+
+test_that("byers_etal_1989 returns message when no usable measures are present", {
+  df <- data.frame(
+    Ind = "ind1",
+    Sex = 1,
+    variable = "Hum1",
+    value = 320
+  )
+
+  res <- byers_etal_1989(df)
+
+  expect_match(
+    res,
+    "bone measurement"
+  )
+})
---
+++ tests/testthat/test-feldesman_etal_1990.R
@@ -0,0 +1,45 @@
+test_that("feldesman_etal_1990 aggregates left/right values and computes stature correctly", {
+  df <- data.frame(
+    Ind = c("OH28", "OH28", "OH28"),
+    Sex = factor('m', 'm', 'm'), # 1 = m, 2 = f, 3 = indet
+    variable = c("Fem1l", "Fem1r", "Tib1"),
+    value = c(456, 456, 380)
+  )
+
+  res <- feldesman_etal_1990(df)
+
+  expect_s3_class(res, "data.frame")
+  expect_equal(rownames(res), "OH28")
+
+  # laterality removed, Fem1 averaged, Tib1 excluded
+  expect_equal(res["OH28", "bone"], "Fem1")
+
+  # n_measures counts both Fem1
+  expect_equal(res["OH28", "n_measures"], 2)
+
+  # expected values:
+  # all:   (456 * 100) / 26.74 = 1705.31
+  tol <- 1
+  expect_true(res["OH28", "if_male"] - 1705 <= tol)
+  expect_true(res["OH28", "if_female"] - 1705 <= tol)
+  expect_true(res["OH28", "if_indet"] - 1705 <= tol)
+
+  # stature should follow the provided sex
+  expect_true(res["OH28", "stature"] - 1705 <= tol)
+})
+
+test_that("feldesman_etal_1990 returns message when no usable measures are present", {
+  df <- data.frame(
+    Ind = "OH28",
+    Sex = 1,
+    variable = "Hum1",
+    value = 320
+  )
+
+  res <- feldesman_etal_1990(df)
+
+  expect_match(
+    res,
+    "bone measurement"
+  )
+})
