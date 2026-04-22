9f6d6fd0f4edc8f6d14c62296d2dc9f9a2b333f0
crinne <crinne@ufg.uni-kiel.de>
2026-04-22 12:01:32 +0200

    note on all_of() > tidyr::all_of()

--- R/readuserdata.R
+++ R/readuserdata.R
@@ -263,7 +263,7 @@ prep.statuaar.data <- function (x, d.form='wide', ind = NA, sex = NA, measures.n
     # wide tabled data pivot to long list
     numcols <- colnames(td)[sapply(td, is.numeric)]
     dl <- tidyr::pivot_longer(td,
-                              cols = all_of(numcols),
+                              cols = tidyr::all_of(numcols),
                               names_to = "variable",
                               values_to = "value",
                               values_drop_na = TRUE)
